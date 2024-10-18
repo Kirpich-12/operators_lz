fe02f70b624e2548223a7f7e1bb74a2d4d775b07
Kirpich-12 <ponchik34@tuta.io>
2024-10-18 11:39:11 +0300

    lst, few

---
+++ sum of series.py
@@ -0,0 +1,31 @@
+from decimal import Decimal as dec
+
+input_data = open('input.txt', 'r')
+data = input_data.read()
+data = data.split()
+
+l = dec(data[0])
+e = int(data[1])
+s = dec(data[2])
+n = 1
+sam = 0             #переменные необходимые для решения
+
+poi = s - l #число которое необходимо добавить к l, следовательно искомая сумма
+s = round(s, e)#
+
+while sam < poi:
+    if poi > 1.65:  #защита от бесконечного цикла
+        print('Уход в бесконечный цикл, замените значение финальной суммы')
+        break
+    sam += (1/ n ** 2)
+    n += 1
+else:
+    n -= 1
+
+#вывод данных(выводит 0 при беск. цикле)
+output_data = open('output.txt', 'w')
+output_data.write(str(n))
+
+#закрытие файлов 
+input_data.close()
+output_data.close()
